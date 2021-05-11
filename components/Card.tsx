080fec54da0789993ad8823cb7717c044f55108e
kumusan <ryogi0200@gmail.com>
2021-05-11 22:40:27 +0900

    position

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,15 +9,18 @@ type Props = {
 };
 
 const Card = (props: Props) => (
-  <div className="flex bg-white p-4 m-10 shadow rounded-lg space-x-4">
+  <div className="flex items-stretch p-4 m-10 shadow rounded-lg space-x-4 bg-whitemode text-black dark:bg-gray-800 dark:text-white">
     {props.detect === "icon" ? (
-      <FontAwesomeIcon icon={props.icon!} className="text-9xl h-full w-full" />
+      <FontAwesomeIcon
+        icon={props.icon!}
+        className="text-9xl h-full w-full justify-self-start"
+      />
     ) : (
       <div className="inline-flex shadow-lg border border-gray-200 rounded-full overflow-hidden h-40 w-40 items-start">
-        <img src={props.img!} alt="" className="h-full w-full" />
+        <img src={props.img!} alt="" className="" />
       </div>
     )}
-    <p className="text-black">test</p>
+    <p className="text-3xl self-center">{props.name}</p>
   </div>
 );
 
