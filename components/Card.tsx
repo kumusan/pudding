e5e65d1ccbf69755b06aa12a8bed352a352be7bf
kumusan <ryogi0200@gmail.com>
2021-05-17 21:42:42 +0900

    add link

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,18 +1,22 @@
 import Props from "../interfaces/card";
 
 const Card = (props: Props) => (
-  <div className="grid p-2 m-10 shadow bg-whitemode text-black max-w-xs dark:bg-gray-800 dark:text-white">
+  <a
+    href={props.link}
+    target="_blank"
+    className="grid p-2 m-10 shadow bg-whitemode text-black max-w-xs dark:bg-gray-800 dark:text-white hover:bg-hoverwhite dark:hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50"
+  >
     {props.detect === "icon" ? (
       <i
         className={`devicon-${props.icon}-plain text-8xl h-auto w-auto justify-self-center`}
       ></i>
     ) : (
-      <div className="shadow-lg border border-gray-200 rounded-full overflow-hidden justify-self-center">
+      <div className="border-gray-200 rounded-full overflow-hidden justify-self-center">
         <img src={props.img!} alt="" className="h-24 w-24" />
       </div>
     )}
     <p className="text-3xl justify-self-center">{props.name}</p>
-  </div>
+  </a>
 );
 
 export default Card;
--- components/Tech.tsx
+++ components/Tech.tsx
@@ -3,18 +3,18 @@ import TechList from "../interfaces/card";
 
 const Tech = () => {
   const techList: TechList[] = [
-    { name: "Vue", icon: "vuejs", link: "" },
-    { name: "React", icon: "react", link: "" },
-    { name: "JavaScript", icon: "javascript", link: "" },
-    { name: "TypeScript", icon: "typescript", link: "" },
-    { name: "Go", icon: "go", link: "" },
-    { name: "Rust", icon: "rust", link: "" },
-    { name: "GraphQL", icon: "graphql", link: "" },
-    { name: "PostgreSQL", icon: "postgresql", link: "" },
-    { name: "Node", icon: "nodejs", link: "" },
-    { name: "Vim", icon: "vim", link: "" },
-    { name: "Laravel", icon: "laravel", link: "" },
-    { name: "Docker", icon: "docker", link: "" },
+    { name: "Vue", icon: "vuejs", link: "https://v3.ja.vuejs.org/" },
+    { name: "React", icon: "react", link: "https://ja.reactjs.org/" },
+    { name: "JavaScript", icon: "javascript", link: "https://developer.mozilla.org/ja/docs/Web/JavaScript" },
+    { name: "TypeScript", icon: "typescript", link: "https://www.typescriptlang.org/" },
+    { name: "Go", icon: "go", link: "https://golang.org/" },
+    { name: "Rust", icon: "rust", link: "https://www.rust-lang.org/" },
+    { name: "GraphQL", icon: "graphql", link: "https://graphql.org/" },
+    { name: "Node", icon: "nodejs", link: "https://nodejs.org/en/" },
+    { name: "Laravel", icon: "laravel", link: "https://laravel.com/" },
+    { name: "Docker", icon: "docker", link: "https://www.docker.com/" },
+    { name: "PostgreSQL", icon: "postgresql", link: "https://www.postgresql.org/" },
+    { name: "Vim", icon: "vim", link: "https://www.vim.org/" },
   ];
   return (
     <div className="m-5 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
@@ -29,5 +29,5 @@ const Tech = () => {
     </div>
   );
 };
-// grid grid-cols-1   gap-4
+
 export default Tech;
