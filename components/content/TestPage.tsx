6fdd3ef132effdea47aaf5c82cce3a9028065a4f
kumusan <ryogi0200@gmail.com>
2021-06-05 11:47:32 +0900

    blog page

---
+++ components/content/TestPage.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => (
+  <div>
+    <h1>title</h1>
+    <h2>day</h2>
+    <p>text</p>
+  </div>
+);
+
+export default TestPage;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,11 @@
 import Layout from "../components/Layout";
 import "tailwindcss/tailwind.css";
+import TestPage from "../components/content/TestPage";
 
 const Blog = () => {
   return (
     <Layout title="KUMUSAN - BLOG">
+      <TestPage />
       <p className="dark:text-white">test</p>
     </Layout>
   );
